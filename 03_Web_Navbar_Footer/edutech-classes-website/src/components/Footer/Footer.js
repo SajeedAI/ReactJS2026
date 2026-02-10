a897f37895f4533af7e799079a8e62c06c1f3478
SajeedAI <training.saif@gmail.com>
2026-02-10 14:27:50 +0530

    v1

---
+++ 03_Web_Navbar_Footer/edutech-classes-website/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2026 Moltres Innovations. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
+
--- 03_Web_Navbar_Footer/edutech-classes-website/src/components/Navbar/Navbar.js
+++ 03_Web_Navbar_Footer/edutech-classes-website/src/components/Navbar/Navbar.js
@@ -4,12 +4,12 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <h2 className="logo">Maie Academy</h2>
+      <h2 className="logo">Maithrivanam Academy</h2>
 
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/classes">Classes</Link></li>
-        <li><Link to="/demo">Demo</Link></li>
+        <li><Link to="/navodaya">Navodaya</Link></li>
         <li><Link to="/login">Login</Link></li>
       </ul>
     </nav>
